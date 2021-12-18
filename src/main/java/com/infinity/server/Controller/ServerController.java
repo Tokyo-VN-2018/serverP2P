68c35707e9c152fed013fe17aa2152a3ab98c05e
tiendung20 <ntdung2316@gmail.com>
2021-12-18 22:17:32 +0700

    Add Socket accept

--- src/main/java/com/infinity/server/Controller/ServerController.java
+++ src/main/java/com/infinity/server/Controller/ServerController.java
@@ -1,9 +1,28 @@
 package com.infinity.server.Controller;
 
+import java.net.ServerSocket;
+
 public class ServerController {
+
+	private ServerSocket ServerSocket;
+
+	private static final int PORT = 7777;
 
 	public ServerController() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public void accept() throws Exception {
+
+		this.ServerSocket = new ServerSocket(PORT);
+
+		try {
+			while (true) {
+				SessionController session = new SessionController(this.ServerSocket.accept());
+			}
+		} finally {
+			this.ServerSocket.close();
+		}
 	}
 
 }
--- src/main/java/com/infinity/server/Controller/SessionController.java
+++ src/main/java/com/infinity/server/Controller/SessionController.java
@@ -1,9 +1,18 @@
 package com.infinity.server.Controller;
 
+import java.net.Socket;
+
 public class SessionController {
+
+	private Socket socket;
 
 	public SessionController() {
 		// TODO Auto-generated constructor stub
 	}
 
+	public SessionController(Socket socket) {
+		// TODO Auto-generated constructor stub
+		this.socket = socket;
+	}
+	
 }
